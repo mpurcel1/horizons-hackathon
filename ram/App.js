f19fac5aa30d6555f4ef5656a077fdf09e3de9c9
git push -u origin <renee8688@gmail.com>
2018-08-03 13:29:32 -0700

    login

--- ram/App.js
+++ ram/App.js
@@ -1,17 +1,201 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  AsyncStorage,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ListView,
+  Alert,
+  Button
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+
+
+class RegisterScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Register'
+  };
+  constructor(){
+    super();
+    this.state = {
+      username:'',
+      password:'',
+      resume:''
+    }
+  }
+  register(){
+    fetch('/register',{
+      method:'POST',
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        username:this.state.username,
+        password:this.state.password,
+        resume:this.state.resume
+      })
+    })
+    .then((response)=>response.json())
+    .then((responseJson) =>{
+      if (responseJson.success){
+        //<Text>{responseJson}</Text>
+        this.props.navigation.navigate('Login');
+      } else{
+        console.log(responseJson);
+      }
+    })
+    .catch((err)=>{
+      console.log('err',err);
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput
+          style={{height:40}}
+          placeholder='Enter your username'
+          onChangeText={(text)=>this.setState({username:text})}
+        />
+        <TextInput
+          style={{height:40}}
+          secureTextEntry={true}
+          placeholder='Enter your password'
+          onChangeText={(text)=>this.setState({password:text})}
+        />
+        <TextInput
+          style={{height:40}}
+          secureTextEntry={true}
+          placeholder='Enter link to your resume'
+          onChangeText={(text)=>this.setState({resume:text})}
+        />
+        <TouchableOpacity onPress={this.register.bind(this)} style={styles.button, styles.buttonRed}>
+          <Text style={styles.textBig}>Submit</Text>
+        </TouchableOpacity>
       </View>
-    );
+    )
   }
 }
+
+class LoginScreen extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      username:'',
+      password:'',
+    }
+  }
+  static navigationOptions = {
+    title: 'Login'
+  };
+
+  login(username,password){
+    return fetch('/login',{
+      method:'POST',
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        username:username,
+        password:password
+      })
+    })
+  }
+
+  checkResponseAndGoToMainScreen(resp){
+    if (resp.success){
+      AsyncStorage.setItem('user',JSON.stringify({
+        username:this.state.username,
+        password:this.state.password
+      }))
+      this.props.navigation.navigate('User');
+    } else{
+      console.log(resp);
+    }
+  }
+
+  componentDidMount(){
+    AsyncStorage.getItem('user')
+      .then(result => {
+        var parsedResult = JSON.parse(result);
+        var username = parsedResult.username;
+        var password = parsedResult.password;
+        if (username && password) {
+          return this.login(username, password)
+            .then(resp => resp.json())
+            .then((respJson)=>this.checkResponseAndGoToMainScreen(respJson));
+        }
+      })
+      .catch(err=>{console.log(err)})
+  }
+
+  press() {
+    return this.login(this.state.username, this.state.password)
+      .then(resp => resp.json())
+      .then((respJson)=>this.checkResponseAndGoToMainScreen(respJson))
+      .catch(err=>{console.log(err)})
+  }
+
+  register() {
+    this.props.navigation.navigate('Register');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textBig}>Login to Hinder!</Text>
+        <TextInput
+          style={{height:40}}
+          placeholder='Enter your username'
+          onChangeText={(text)=>this.setState({username:text})}
+        />
+        <TextInput
+          style={{height:40}}
+          secureTextEntry={true}
+          placeholder='Enter your password'
+          onChangeText={(text)=>this.setState({password:text})}
+        />
+        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+          <Text style={styles.buttonLabel}>Tap to Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
+          <Text style={styles.buttonLabel}>Tap to Register</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+class UserScreen extends React.Component {
+  render(){
+    return (
+      <View></View>
+    )
+  }
+}
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <RegisterScreen/>
+//       </View>
+//     );
+//   }
+// }
+
+export default StackNavigator({
+  Login: {
+    screen: LoginScreen,
+  },
+  Register: {
+    screen: RegisterScreen,
+  },
+  User: {
+  screen:UserScreen,
+},
+}, {initialRouteName: 'Register'})
 
 const styles = StyleSheet.create({
   container: {
